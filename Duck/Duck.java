89ba97333ff3c3535149c447b896495aae9032fb
mingadinga <minpearl0826@gmail.com>
2023-01-05 19:53:57 +0900

    오리 테스트 - 수정자 주입

--- Duck/Duck.java
+++ Duck/Duck.java
@@ -1,6 +1,15 @@
-public abstract class Duck {    
-    FlyBehavior flyBehavior;    
-    QuackBehavior quackBehavior;  
+public abstract class Duck {
+
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+
+    public void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+
+    public void setQuackBehavior(QuackBehavior quackBehavior) {
+        this.quackBehavior = quackBehavior;
+    }
 
     public Duck() { }	     
     public abstract void display();    
